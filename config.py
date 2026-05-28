a33b288937f11dc6e742674f5949667dc30c122d
Ana Rebeca Perez Garcia <anegir07784@gmail.com>
2026-05-28 16:01:28 -0600

    Fix: alinear nombre de bd a pag_carni

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ def get_connection():
         port=3306,              # Puerto interno por defecto de MySQL en Docker
         user="root",            # El usuario que definiste en docker-compose (suele ser root)
         password="rootpassword",# La contraseña de tu docker-compose
-        database="carniceria_db", # O el nombre exacto de la BD en tu docker-compose
+        database="pag_carni", # O el nombre exacto de la BD en tu docker-compose
         charset="utf8mb4"
     )
